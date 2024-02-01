afdd62f24c6324ca8cef0287b433054ef46f4df8
Eduardo Dalla Vecchia <eduardo@MacBook-Pro-de-Eduardo.local>
2024-02-01 19:08:08 -0300

    Sidebar steps done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,16 +4,9 @@
     class="tw-font-primary tw-bg-magnolia tw-flex tw-items-center tw-justify-center"
   >
     <q-page-container class="tw-container tw-p-3">
-      <div class="tw-bg-white tw-shadow-md tw-flex">
-        <aside class="tw-min-w-60">
-          steps
-        </aside>
-        <router-view class="tw-flex-1" />
-      </div>
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,186 @@
 <template>
   <q-page :style-fn="boxHeight">
-    content
+    <div class="tw-bg-white tw-shadow-md tw-rounded-xl tw-overflow-hidden">
+      <q-stepper
+        v-model="step"
+        ref="stepper"
+        class="tw-flex tw-p-4 tw-rounded-none"
+        header-class="custom-steps"
+      >
+        <q-step
+          :name="1"
+          title="step 1"
+          caption="Your info"
+          :done="done1"
+          color="transparent"
+          active-icon="none"
+        >
+          <div class="tw-min-h-[480px] tw-flex tw-flex-col tw-justify-between">
+            <div class="">
+              step1
+            </div>
+            <q-stepper-navigation class="tw-flex tw-justify-between">
+              <div>
+                <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+              </div>
+              <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 4 ? 'Confirm' : 'Next Step'" />
+            </q-stepper-navigation>
+          </div>
+        </q-step>
+
+        <q-step
+          :name="2"
+          title="step 2"
+          caption="Select plan"
+          :done="done2"
+          color="transparent"
+          active-icon="none"
+        >
+        <div class="tw-min-h-[480px] tw-flex tw-flex-col tw-justify-between">
+            <div class="">
+              step2
+            </div>
+            <q-stepper-navigation class="tw-flex tw-justify-between">
+              <div>
+                <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+              </div>
+              <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 4 ? 'Confirm' : 'Next Step'" />
+            </q-stepper-navigation>
+          </div>
+        </q-step>
+
+        <q-step
+          :name="3"
+          title="step 3"
+          caption="Add-ons"
+          :done="done3"
+          color="transparent"
+          active-icon="none"
+        >
+          <div class="tw-min-h-[480px] tw-flex tw-flex-col tw-justify-between">
+            <div class="">
+              step3
+            </div>
+            <q-stepper-navigation class="tw-flex tw-justify-between">
+              <div>
+                <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+              </div>
+              <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 4 ? 'Confirm' : 'Next Step'" />
+            </q-stepper-navigation>
+          </div>
+        </q-step>
+
+        <q-step
+          :name="4"
+          title="step 4"
+          caption="Summary"
+          :done="done2"
+          color="transparent"
+          active-icon="none"
+        >
+        <div class="tw-min-h-[480px] tw-flex tw-flex-col tw-justify-between">
+            <div class="">
+              step4
+            </div>
+            <q-stepper-navigation class="tw-flex tw-justify-between">
+              <div>
+                <q-btn v-if="step > 1" flat color="primary" @click="$refs.stepper.previous()" label="Back" class="q-ml-sm" />
+              </div>
+              <q-btn @click="$refs.stepper.next()" color="primary" :label="step === 4 ? 'Confirm' : 'Next Step'" />
+            </q-stepper-navigation>
+          </div>
+        </q-step>
+      </q-stepper>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-    function boxHeight () {
-      return { minHeight: 0 }
-    }
+  import { ref } from 'vue';
+
+  const step = ref(1)
+  const done1 = ref(false)
+  const done2 = ref(false)
+  const done3 = ref(false)
+
+  function boxHeight () {
+    return { minHeight: 0 }
+  }
 </script>
+
+<style lang="scss">
+  .custom-steps {
+    flex-direction: column;
+    background: url('../assets/bg-sidebar-desktop.svg') left bottom no-repeat;
+    background-size: cover;
+    border-radius: 0.5rem;
+    text-transform: uppercase;
+    width: 260px;
+    padding: 30px 10px;
+    display: block;
+    counter-reset: css-counter;
+
+    & ~ div {
+      flex: 1;
+    }
+
+    .q-stepper__dot::before,
+    .q-stepper__label::after {
+      display: none;
+    }
+
+    .q-stepper__dot {
+      width: 33px;
+      height: 33px;
+      margin-right: 16px;
+      margin-bottom: -5px;
+      font-weight: bold;
+      border: 1px solid #fff;
+
+      span {
+        i {
+          display: none;
+        }
+
+        &::before {
+          counter-increment: css-counter;
+          content: counter(css-counter)
+        }
+      }
+    }
+
+    .q-stepper__tab {
+      min-height: 0;
+      margin-bottom: 14px;
+      justify-content: flex-start;
+
+      &:last-child {
+        justify-content: flex-start;
+      }
+
+      &--active {
+        .q-stepper__dot {
+          border-color: hsl(206, 94%, 87%);
+          background-color: hsl(206, 94%, 87%);
+
+          span {
+            color: hsl(213, 96%, 18%);
+          }
+        }
+      }
+
+      .q-stepper__title {
+        font-size: 12px;
+        line-height: 1.8;
+        color: hsl(228, 100%, 84%);
+      }
+
+      .q-stepper__caption {
+        color: #fff;
+        font-size: 14px;
+        font-weight: 500;
+        letter-spacing: 1px;
+      }
+    }
+  }
+</style>
