739aa0a732297a8dffc09f0eb145089275160e1a
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2025-04-27 03:08:32 -0300

    chore: update quasar.config.js deployment configuration

--- quasar.config.js
+++ quasar.config.js
@@ -75,7 +75,7 @@ module.exports = configure(function (/* ctx */) {
       // ignorePublicFolder: true,
       // minify: false,
       // polyfillModulePreload: true,
-      // distDir
+      distDir: 'dist/spa',
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
