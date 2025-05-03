4543ab974ac8a843cb699973f37fc463fa8dcd61
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2025-05-02 22:55:46 -0300

    chore: Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 100,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  vueIndentScriptAndStyle: true
+};
