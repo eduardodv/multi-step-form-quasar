5f4b9fa57577ffbc61082d1477e7edd75787929c
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2025-05-03 01:19:42 -0300

    refactor: Set max container size in main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     view="lHh Lpr lFf"
     class="tw-font-primary tw-bg-magnolia tw-flex tw-items-center tw-justify-center"
   >
-    <q-page-container class="tw-container tw-p-3">
+    <q-page-container class="tw-container tw-max-w-[960px] tw-p-3">
       <router-view />
     </q-page-container>
   </q-layout>
