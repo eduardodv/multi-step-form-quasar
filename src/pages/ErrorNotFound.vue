08c552f1c5768a44e13f82f2f8ce0fff51ea2a94
Eduardo Dalla Vecchia <eduardodvecchia@gmail.com>
2025-05-03 01:25:52 -0300

    refactor: Changing the error page layout

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="fullscreen text-center q-pa-md flex flex-center tw-font-primary tw-bg-magnolia">
+  <div
+    class="fullscreen text-center q-pa-md flex flex-center tw-font-primary tw-bg-magnolia tw-text-marine-blue"
+  >
     <div>
-      <div style="font-size: 30vh" class="tw-text-marine-blue">404</div>
+      <h2 class="tw-text-8xl sm:tw-text-9xl tw-font-extrabold">404</h2>
 
-      <div class="text-h2" style="opacity: 0.6">Oops. Nothing here...</div>
+      <h1 class="tw-text-3xl sm:tw-text-5xl tw-mt-10 tw-mb-16">Oops. Nothing here...</h1>
 
       <q-btn
-        class="q-mt-xl tw-bg-marine-blue tw-text-white"
+        class="tw-bg-purplish-blue tw-text-white tw-w-32 tw-h-12 tw-text-base"
         unelevated
         to="/"
-        label="Go Home"
+        label="Go Form"
         no-caps
       />
     </div>
